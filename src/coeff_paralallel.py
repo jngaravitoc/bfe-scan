29be532c67351d5ce76b43f4293a074b16f6d6ad
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-10-01 15:49:25 -1000

    function that write the coefficients

--- src/coeff_paralallel.py
+++ src/coeff_paralallel.py
@@ -1,10 +1,18 @@
+"""
+TODO: 
+    - Test speed of the code and compare with that one of the BFE-c
+    - Write Coefficients function
+    - 
+
+"""
+
 import numpy as np
 import schwimmbad 
 from gala.potential.scf._computecoeff import STnlm_discrete, STnlm_var_discrete
+import read_snap
 
 
 class Coeff_parallel(object):
-
     def __init__(self, pos, mass, r_s):
         self.pos = pos
         self.mass = mass
@@ -13,8 +21,6 @@ class Coeff_parallel(object):
         self.s = self.r / r_s
         self.phi = np.arctan2(np.ascontiguousarray(self.pos[:,1]), np.ascontiguousarray(self.pos[:,0]))
         self.X = np.ascontiguousarray(self.pos[:,2]) / self.r
-
-
 
     def compute_coeffs_discrete_parallel(self, n, l, m):
         S, T = STnlm_discrete(self.s, self.phi, self.X, self.mass, n, l, m)
@@ -44,9 +50,9 @@ def main(pool, pos, mass, nmax, lmax, r_s):
     
     results = pool.map(worker, tasks)
         
-    return results
     pool.close()
 
+    return results
 
 def coeff_matrix(STnlm):
     Snlm_matrix = np.zeros((nmax+1, lmax+1, lmax+1))
@@ -60,6 +66,30 @@ def coeff_matrix(STnlm):
                 k+=1
     return Snlm_matrix, Tnlm_matrix
 
+def write_coefficients(filename, Snlm, varSnlm, Tnlm, varTnlm, nmax, lmax, r_s):
+    """
+    
+    """
+    Nrows = (nmax+1)*(lmax+1)*(lmax+1)
+
+    data = np.array([Snlm.flatten(), varSnlm.flatten(), Tnlm.flatten(),
+                         varTnlm.flatten()]).T
+    header = 'nmax: {:d}, lmax:{:d}, r_s:{:f}'.format(nmax, lmax, r_s)
+    np.savetxt(filename, data, header=header)
+
+    
+
+def compute_coeff_parallel(pos, nmax, lmax, r_s, ncores):
+    """
+
+    """
+    #pool = schwimmbad.choose_pool(mpi=False, processes=ncores)
+    pool = schwimmbad.choose_pool(mpi=True, processes=2)
+    results = main(pool, pos, mass, nmax, lmax, r_s)
+    Snlm, Tnlm = coeff_matrix(results)
+    return Snlm, Tnlm
+
+
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser(description="")
@@ -72,11 +102,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     pool = schwimmbad.choose_pool(mpi=args.mpi, processes=args.n_cores)
-    pos = np.random.random((100, 3))
-    mass = np.ones(100)
-    nmax = 4
-    lmax = 4
+    snap_name = '../../MW_anisotropy/code/test_snaps/LMC3_507K_part_b1_091.txt'
+    pos, mass = read_snap.read_data(snap_name)
+    nmax = 5
+    lmax = 5
     r_s = 10
+    
     results = main(pool, pos, mass, nmax, lmax, r_s)
+    
     Snlm, Tnlm = coeff_matrix(results)
-    print(Snlm[0,0,0], Tnlm[0,0,0])
+    
+    write_coefficients("test_coefficients.txt", Snlm, Snlm, Tnlm, Tnlm, nmax, lmax, r_s)
