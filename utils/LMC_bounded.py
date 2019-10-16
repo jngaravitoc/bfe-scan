d7055db4cbb91c3cf367296d7b2944b3fb560f1d
jngaravitoc <jngaravitoc@email.arizona.edu>
2019-10-15 19:10:43 -0700

    updated to gala and for printing also velocities

--- utils/LMC_bounded.py
+++ utils/LMC_bounded.py
@@ -10,7 +10,8 @@ usage: python LMC_bounded.py filename nmax lmax rs
 
 
 import numpy as np
-import biff
+from gala.potential.scf import compute_coeffs_discrete
+from gala.potential.scf._bfe import potential as gala_scf_potential
 import sys
 
 def reading_particles(snap_name):
@@ -24,14 +25,27 @@ def reading_particles(snap_name):
 
 def compute_scf_pot(pos, rs, nmax, lmax, mass):
     G_gadget = 43007.1
-    S, T = biff.compute_coeffs_discrete(np.ascontiguousarray(pos).astype(float),
+    S, T = compute_coeffs_discrete(np.ascontiguousarray(pos).astype(float),
                                         mass, nmax, lmax, rs)
 
-    LMC_potential = biff.potential(np.ascontiguousarray(pos), S, T, M=1, r_s=rs, G=G_gadget)
+    LMC_potential = gala_scf_potential(np.ascontiguousarray(pos), S, T, M=1, r_s=rs, G=G_gadget)
     return LMC_potential
 
 
-def bound_particles(pot, pos, vel, ids):
+def compute_scf_pot_parallel(pos, rs, nmax, lmax, mass):
+    """
+
+    """
+    G_gadget = 43007.1
+    print('Computing coefficients')
+    S, T = compute_coeffs_discrete(np.ascontiguousarray(pos).astype(float),
+                                        mass, nmax, lmax, rs)
+    print('Computing potential')
+    LMC_potential = gala_scf_potential(np.ascontiguousarray(pos), S, T, M=1, r_s=rs, G=G_gadget)
+    return LMC_potential
+
+
+def bound_particles(pot, pos, vel):
     vmag_lmc = np.sqrt(vel[:,0]**2 + vel[:,1]**2 + vel[:,2]**2)
     dist_lmc = np.sqrt(pos[:,0]**2 + pos[:,1]**2 + pos[:,2]**2)
     T = vmag_lmc**2/2
@@ -39,27 +53,27 @@ def bound_particles(pot, pos, vel, ids):
 
     lmc_bound = np.where(T+V<=0)[0]
 
-    return pos[lmc_bound], vel[lmc_bound], ids[lmc_bound]
+    return pos[lmc_bound], vel[lmc_bound]
 
 
-def find_bound_particles(pos, vel, mass, ids, rs, nmax, lmax):
+def find_bound_particles(pos, vel, mass, rs, nmax, lmax):
     N_init = len(pos)
     pot = compute_scf_pot(pos, rs, nmax, lmax, mass)
-    pos_bound, vel_bound, ids_bound = bound_particles(pot, pos, vel, ids)
+    pos_bound, vel_bound = bound_particles(pot, pos, vel)
     N_bound = len(pos_bound)
 
     print('Initial number of particles:', N_init)
     i=0
     while (np.abs(N_init-N_bound) > (0.01*N_init)):
         pot = compute_scf_pot(pos_bound, rs, nmax, lmax, mass)
-        pos_bound, vel_bound, ids_bound = bound_particles(pot, pos_bound, vel_bound, ids_bound)   
+        pos_bound, vel_bound = bound_particles(pot, pos_bound, vel_bound)   
         N_init = N_bound
         N_bound = len(pos_bound)
         i+=1
         print(N_init, N_bound)
         print('Number of bound particles: {} iteration: {}'.format(N_bound, i))
 
-    return pos_bound, vel_bound, N_bound, ids_bound
+    return pos_bound, vel_bound, N_bound
 
 
 if __name__ == "__main__":
@@ -85,11 +99,12 @@ if __name__ == "__main__":
                                                          lmax)
     print('Bound particles computed')
     
-    
+    mass_bound = np.ones(len(ids_bound))
     print(pos_bound[0])
     print(vel_bound[0])
     lmc_bound = np.array([pos_bound[:,0], pos_bound[:,1], pos_bound[:,2], 
-                          vel_bound[:,0], vel_bound[:,1], vel_bound[:,2]]).T
+                          vel_bound[:,0], vel_bound[:,1], vel_bound[:,2],
+                          mass_bound]).T
     
     np.savetxt(out_name, lmc_bound)
     
