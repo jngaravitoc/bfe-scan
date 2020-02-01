1d24ebf91fda32b6ff165238ae71a0263643821e
jngaravitoc <jngaravitoc@email.arizona.edu>
2020-01-31 19:13:29 -1000

    adding new features

--- src/allvars.py
+++ src/allvars.py
@@ -20,6 +20,16 @@ def readparams(paramfile):
     initSnap = d["initSnap"]
     finalSnap = d["finalSnap"]
     SatBFE = d["SatBFE"] 
+    sat_rs = d["Satrs"]
+    nmax_sat = d["nmaxSat"]
+    lmax_sat = d["nmaxSat"]
+    HostBFE = d["HostBFE"]
+    SatBoundParticles = d["SatBoundParticles"]
+    HostSatUnboundPart = d["HostSatUnboundPart"]
+    write_snaps_ascii = d["WriteSnapsAscii"]
+    out_ids_bound_unbound_sat = d["OutIdsBoundUnbound"]
+    plot_scatter_sample = d["PlotScatterSample"]
+
 
     assert type(inpath)==str, "inpath parameter  must be a string"
     assert type(snapname)==str, "snapname parameter must be a string"
@@ -39,5 +49,8 @@ def readparams(paramfile):
     assert type(SatBFE)==int, "SatBFE parameter must be an integer"
 
     return [inpath, snapname, outpath, outname, npartHalo, samplePart, nmax,
-            lmax, rs, ncores, mpi, rhaloCut, initSnap, finalSnap, SatBFE]
+            lmax, rs, ncores, mpi, rhaloCut, initSnap, finalSnap, SatBFE,
+            sat_rs, nmax_sat, lmax_sat, HostBFE, SatBoundParticles,
+            HostSatUnboundPart, write_snaps_ascii, out_ids_bound_unbound_sat, 
+            plot_scatter_sample]
 
