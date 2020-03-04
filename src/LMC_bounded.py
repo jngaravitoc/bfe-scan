fixed bug when doing the n, m, l lists for parallel computation

src/LMC_bounded.py
@@ -1,6 +1,6 @@
 """
 Script to found the bound particles of a satellite
-galaxy.
+galaxy using the BFE method.
 
 author: Nicolás Garavito-Camargo
 
@@ -18,6 +18,19 @@ def parallel_potential_batches(pos, S, T, rs, nmax, lmax, G, ncores,
     """
     Function to compute potential in parallel. To avoid 
     large memory errors the particle's potential is computed in batches.
+
+    Parameters:
+    ----------
+    pos
+    S : cosine components
+    T : sine components
+    rs : halo scale length
+    nmax : maximum of n number in expansion
+    lmax : maximum l number in expansion
+    G : value of gravitational constant
+    ncores : number of cores used to compute the potential in parallel
+    npart_sample : number of particles 
+
     """
     
     Nparticles = len(pos[:,0])
@@ -25,34 +38,43 @@ def parallel_potential_batches(pos, S, T, rs, nmax, lmax, G, ncores,
     nbatches = int(Nparticles/npart_sample)
     print('computing potential in parallel in {} batches of particles'.format(nbatches))
     pot_all = np.array([])
+
     if nbatches < 1:
       pos_batch = pos
-      halo_pot = parallel_pot.PBFEpot(pos_batch, S, T, rs, nmax, lmax, G=43007.1, M=1)
+      halo_pot = parallel_pot.PBFEpot(pos_batch, S, T, rs, nmax, lmax, G=G, M=1)
+
+      # choosing pool to compute the potential in parallel
       pool2 = schwimmbad.choose_pool(mpi=False, processes=ncores)
+      # Compute potential in parallel
       pot_all = halo_pot.main(pool2)
 
     else :
         for i in range(nbatches):
+            # making batches
             if i < (nbatches-1):
                 pos_batch = pos[i*npart_sample:(i+1)*npart_sample]
             elif i == (nbatches-1):
                 pos_batch = pos[i*npart_sample:]
             else :
                 pos_batch = pos
+
             print("Done batch {}".format(i))
-            halo_pot = parallel_pot.PBFEpot(pos_batch, S, T, rs, nmax, lmax, G=43007.1, M=1)
+            halo_pot = parallel_pot.PBFEpot(pos_batch, S, T, rs, nmax, lmax, G=G, M=1)
             pool2 = schwimmbad.choose_pool(mpi=False, processes=ncores)
             pot = halo_pot.main(pool2)
             pot_all = np.hstack((pot_all, pot))
-            del(pot)
-            del(pos_batch)
+
+            # clean memory
+            del(pot, pos_batch, pool2)
+    # clean memory
+    del(pos)
 
     assert (len(pot_all)==Nparticles), 'Hey some potentials are missing here'
     return pot_all
 
 def compute_scf_pot(pos, rs, nmax, lmax, mass, ncores, npart_sample):
     """
-    TODO: Parallelize this function here!
+    
     """
     # Compute coefficients
     # Compute potential
@@ -60,16 +82,18 @@ def compute_scf_pot(pos, rs, nmax, lmax, mass, ncores, npart_sample):
 
     # Compute coefficients of bound particles
     pool = schwimmbad.choose_pool(mpi=False, processes=ncores)
-    assert len(pos)==len(mass), 'positions arrays and massed do not match'
+    assert len(pos)==len(mass), 'position array and mass array length do not match'
     halo_coeff = cop.Coeff_parallel(pos, mass, rs, False, nmax, lmax)
     results = halo_coeff.main(pool)
     S = results[:,0]
     T = results[:,1]
-     
+
+    del(results, halo_coeff, pool)
+
     # Compute potential
     pot = parallel_potential_batches(pos, S, T, rs, nmax, lmax, 
                                      43007.1, ncores, npart_sample)
-    
+    del(S,T,pos)
     print("Done computing parallel potential")
     return pot
 
@@ -84,9 +108,7 @@ def bound_particles(pot, pos, vel, ids):
 
     lmc_bound = np.where(T+V<=0)[0]
     lmc_unbound = np.where(T+V>0)[0]
-    del(T)
-    del(V)
-    del(vmag_lmc)
+    del(T, V, vmag_lmc)
 
     return pos[lmc_bound], vel[lmc_bound], ids[lmc_bound], pos[lmc_unbound], vel[lmc_unbound], ids[lmc_unbound]
 

src/parallel_pot.py
@@ -50,8 +50,9 @@ class PBFEpot:
         self.nmax = nmax
         self.lmax = lmax
         self.r = (self.pos[:,0]**2 + self.pos[:,1]**2 + self.pos[:,2]**2)**0.5 
-        self.theta = np.arccos(self.pos[:,2]/self.r)
-        self.phi = np.arctan2(self.pos[:,1], self.pos[:,0]) + np.pi
+        self.theta = np.arccos(self.pos[:,2]/self.r) # cos(theta)
+
+        self.phi = np.arctan2(self.pos[:,1], self.pos[:,0]) 
         self.s = self.r/self.rs
         self.G = G
         self.M = M
@@ -64,8 +65,8 @@ class PBFEpot:
         l_list = np.zeros(ncoeff)
         m_list = np.zeros(ncoeff)
         i=0
-        for n in range(nmax):
-            for l in range(lmax):
+        for n in range(nmax+1):
+            for l in range(lmax+1):
                 for m in range(l+1):
                     n_list[i] = n 
                     l_list[i] = l
@@ -78,10 +79,11 @@ class PBFEpot:
     
         phi_l = -s**l * (1+s)**(-2*l-1)
         #phi_nlm = special.eval_gegenbauer(n, 2*l+1.5, (s-1)/(s+1)) * special.sph_harm(m, l, 0, theta)
-        phi_nlm = special.eval_gegenbauer(n, 2*l+1.5, (s-1)/(s+1)) * special.lpmv(m, l, np.cos(theta))
+        phi_nlm = special.eval_gegenbauer(n, 2*l+1.5, ((s-1)/(s+1))) * special.lpmv(m, l, np.cos(theta))
         factor = ((2*l+1) * math.factorial(l-m)/math.factorial(l+m))**0.5
+        #factor=1
         #factor = 1#(4*np.pi)**0.5
-        return  factor*phi_l*phi_nlm 
+        return  factor*phi_l*phi_nlm #np.sqrt(4*np.pi)
 
     
     def potential(self, task):
@@ -90,13 +92,18 @@ class PBFEpot:
         nlist, llist, mlist, S, T = task
         #pot = np.zeros(len(nlist))
         #for i in range(len(self.S)):
-        pot = self.bfe_pot(nlist, llist, mlist, self.s, self.theta)*(S*np.cos(mlist*self.phi)+T*np.sin(mlist*self.phi))
+        pot = self.bfe_pot(nlist, llist, mlist, self.s, self.theta)\
+                * (S*np.cos(mlist*self.phi)+T*np.sin(mlist*self.phi))
         return pot*self.G*self.M/self.rs
     
     def main(self, pool):
         nlist, llist, mlist = self.nlm_list(len(self.S), self.nmax, self.lmax)
+        #rint(self.S, self.T)
         tasks = list(zip(nlist, llist, mlist, self.S, self.T))
         results = pool.map(self.potential, tasks)
+        #for i in range(len(nlist)):
+        #    results = self.potential([nlist[i], llist[i], mlist[i], self.S[i], self.T[i]])
+        print(np.sum(results, axis=0))
         pool.close()
         return np.sum(results, axis=0)
 
@@ -117,6 +124,4 @@ if __name__ == "__main__":
     r_arr = np.array(r)
     r_all = np.sum(r_arr, axis=0)
     t2 = time.time()
-    print("total time = {:.3f}s".format(t2-t1))
-    print(r_all)
     print("Done")