1d0109e53cca842caa4d43d7828f0c8d54d16fff
jngaravitoc <jngaravitoc@email.arizona.edu>
2020-05-27 19:56:30 -1000

    tests

--- tests/test_com.py
+++ tests/test_com.py
@@ -1,10 +1,10 @@
 import numpy as np
 import sys
-sys.path.append('../src/')
-import com
+sys.path.append('../bfe-py/')
+from bfe import com
 
 def test_com_halo():
-    filename = "./data/plummer_sphere_10K.txt"
+    filename = "/home/xzk/work/github/bfe-py/tests/data/plummer_sphere_10K.txt"
     data = np.loadtxt(filename)
     pos = data[:,0:3]
     vel = data[:,3:6]
@@ -18,7 +18,7 @@ def test_com_halo():
 
 
 def test_com_shrinking_sphere_halo():
-    filename = "./data/plummer_sphere_10K.txt"
+    filename = "/home/xzk/work/github/bfe-py/tests/data/plummer_sphere_10K.txt"
     data = np.loadtxt(filename)
     pos = data[:,0:3]
     vel = data[:,3:6]
@@ -27,7 +27,9 @@ def test_com_shrinking_sphere_halo():
     rcom_true = np.array([-0.05291256, -0.10510422,  0.18606588])
     vcom_true = np.array([-3.63038043,  0.33830252,  2.06705637])
 
-    assert np.allclose(rcom, rcom_true, rtol=1e-2)
-    assert np.allclose(vcom, vcom_true, rtol=1e-2)
+    assert np.allclose(rcom, rcom_true, rtol=1e-2), \
+            """bfe-py.com.shrinking_sphere is failing """
+    assert np.allclose(vcom, vcom_true, rtol=1e-2), \
+            """bfe-py.com.shrinking_sphere is failing """
 
 
