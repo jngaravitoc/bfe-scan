68f45cc5bdba84cea56778539e9d8fda1cd3d73f
jngaravitoc <jngaravitoc@email.arizona.edu>
2021-03-18 14:30:02 -0700

    added simple density plot function

--- bfe/pipeline/quick_viz_check.py
+++ bfe/pipeline/quick_viz_check.py
@@ -22,4 +22,15 @@ def scatter_plot(snap, data):
     plt.close()
 
 
-
+def density_plot(snap, data):
+    x = data[:,0]
+    y = data[:,1]
+    z = data[:,2]
+    r = np.sqrt(np.sum(data**2, axis=1))
+    index_cut = np.where((r<300) & (np.abs(x<5)))[0]
+    hist_slice = np.histogram2d(y[index_cut], z[index_cut], 100)
+    fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    im1= ax.imshow(np.log10(np.abs(hist_slice[0].T)), origin='lower',  cmap='coolwarm', extent=[-300, 300, -300, 300], vmin=0, vmax=3)
+    ax.set_xlim(-200, 200)
+    ax.set_ylim(-200, 200)
+    plt.savefig(snap+"_yz_density.png", bbox_inches='tight')
